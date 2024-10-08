1dcbdef526ff6f062f73b0d89e5b3fc828d56fd1
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-10-08 23:36:14 +0200

    bump version 0.1.0 -> 0.1.1

--- PersistDict/PersistDict.py
+++ PersistDict/PersistDict.py
@@ -23,7 +23,7 @@ except ImportError:
 
 @typechecker
 class PersistDict(dict):
-    __VERSION__: str = "0.1.0"
+    __VERSION__: str = "0.1.1"
     __already_called__: bool = False
     __missing_value__: Any = MISSING
 
